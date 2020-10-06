36c141c50e34f6aeb661c2460b16e2d0eb46dc18
dolomiten213 <56438618+dolomiten213@users.noreply.github.com>
2020-10-06 20:11:15 +0300

    task 1 (#1)
    * Everything is ready
    
    * Added sample program.cs
    
    * Added test onnx model resnet34-v1-7
    
    * Create README.md
    
    * Added requested changes
    
    * Struct as result & exception trace output
    
    * Program.cs updated
    
    Co-authored-by: dolomiten <dolomiten213@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# s02170203
+Repo for practical .NET programming 2020 tasks (student account s02170203)
+In samples you can find test Program.cs & folder with images.
+In developing I used onnx model resnet152-v2-7 (https://github.com/onnx/models/blob/master/vision/classification/resnet/model/resnet152-v2-7.onnx).
+This onnx model file is to large to add it in repository. I added resnet34-v1-7.onnx instead. It's smaller and works 2x faster, but recognition results should be worse.
---
+++ samples/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using ImageRecognition;
+
+namespace Launcher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string imagesPath = (args.Length == 0) ? @"D:\Pictires\Desktop" : args[0];
+
+            ImageRecognizer.onnxModelPath = (args.Length == 2) ? args[1]: "resnet34-v1-7.onnx";
+
+            ImageRecognizer.ImageRecognizerResultUpdate += OutputRecognitionHandler;
+
+            await ImageRecognizer.RecognitionAsync(imagesPath);
+
+        }
+        static void OutputRecognitionHandler(Prediction s)
+        {
+            Console.WriteLine($"{s.Path, 80} {s.Label, 15} {s.Confidence, 15}");
+        }
+    }
+}
